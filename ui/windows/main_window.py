32234db252c9c9d1cdd43fa1f13fc2eab0fbe689
molodozhenia-nazar-university <6868491@stud.kai.edu.ua>
2025-08-31 21:12:17 +0300

    fix: виправлено стан вікна v0.1.1

--- ui/windows/main_window.py
+++ ui/windows/main_window.py
@@ -1,5 +1,4 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
-from PyQt6.QtGui import QScreen
+from PyQt6.QtWidgets import QMainWindow
 from ui.styles.main_style import MAIN_WINDOW_STYLES
 
 
@@ -9,20 +8,6 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("Експертна система")
-        self.setFixedSize(500, 500)
-        self.center_window()
         self.setStyleSheet(MAIN_WINDOW_STYLES)
-
-        # self.setup_ui()
-
-    def center_window(self):
-
-        # Отримуємо геометрію екрана
-        screen_geometry = QApplication.primaryScreen().geometry()
-
-        # Розраховуємо центр екрана
-        x = (screen_geometry.width() - self.width()) // 2
-        y = (screen_geometry.height() - self.height()) // 2
-
-        # Встановлюємо позицію
-        self.move(x, y)
+        self.setMinimumSize(800, 600)
+        self.showMaximized()
